71f0d25e41fd1d6147275c83a23bd79045ce8d12
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-10-10 21:46:08 +0100

    Updated Travis-CI to compile KLEE with CMake only

--- .travis/klee.sh
+++ .travis/klee.sh
@@ -25,34 +25,19 @@ source ${SRC_DIR}/.travis/solvers.sh
 KLEE_Z3_CONFIGURE_OPTION=""
 SOLVER_LIST=$(echo "${SOLVERS}" | sed 's/:/ /')
 
-if [ "X${USE_CMAKE}" == "X1" ]; then
-  # Set CMake configure options
-  for solver in ${SOLVER_LIST}; do
-    echo "Setting CMake configuration option for ${solver}"
-    case ${solver} in
-    Z3)
-      echo "Z3"
-      KLEE_Z3_CONFIGURE_OPTION="-DENABLE_SOLVER_Z3=TRUE"
-      ;;
-    *)
-      echo "Unknown solver ${solver}"
-      exit 1
-    esac
-  done
-else
-  for solver in ${SOLVER_LIST}; do
-    echo "Setting configuration option for ${solver}"
-    case ${solver} in
-    Z3)
-      echo "Z3"
-      KLEE_Z3_CONFIGURE_OPTION="--with-z3=/usr"
-      ;;
-    *)
-      echo "Unknown solver ${solver}"
-      exit 1
-    esac
-  done
-fi
+# Set CMake configure options
+for solver in ${SOLVER_LIST}; do
+  echo "Setting CMake configuration option for ${solver}"
+  case ${solver} in
+  Z3)
+    echo "Z3"
+    KLEE_Z3_CONFIGURE_OPTION="-DENABLE_SOLVER_Z3=TRUE"
+    ;;
+  *)
+    echo "Unknown solver ${solver}"
+    exit 1
+  esac
+done
 
 ###############################################################################
 # Compile KLEE and run tests
@@ -60,88 +45,43 @@ fi
 mkdir klee-build
 cd klee-build
 
-if [ "X${USE_CMAKE}" == "X1" ]; then
-  KLEE_UCLIBC_CONFIGURE_OPTION="-DENABLE_KLEE_UCLIBC=TRUE -DKLEE_UCLIBC_PATH=${SRC_DIR} -DENABLE_POSIX_RUNTIME=TRUE"
+KLEE_UCLIBC_CONFIGURE_OPTION="-DENABLE_KLEE_UCLIBC=TRUE -DKLEE_UCLIBC_PATH=${SRC_DIR} -DENABLE_POSIX_RUNTIME=TRUE"
+
+GTEST_SRC_DIR="${BUILD_DIR}/test-utils/googletest-release-1.7.0/"
+if [ "X${DISABLE_ASSERTIONS}" == "X1" ]; then
+  KLEE_ASSERTS_OPTION="-DENABLE_KLEE_ASSERTS=FALSE"
 else
-  KLEE_UCLIBC_CONFIGURE_OPTION="--with-uclibc=${SRC_DIR} --enable-posix-runtime"
+  KLEE_ASSERTS_OPTION="-DENABLE_KLEE_ASSERTS=TRUE"
 fi
 
-if [ "X${USE_CMAKE}" == "X1" ]; then
-  GTEST_SRC_DIR="${BUILD_DIR}/test-utils/googletest-release-1.7.0/"
-  if [ "X${DISABLE_ASSERTIONS}" == "X1" ]; then
-    KLEE_ASSERTS_OPTION="-DENABLE_KLEE_ASSERTS=FALSE"
-  else
-    KLEE_ASSERTS_OPTION="-DENABLE_KLEE_ASSERTS=TRUE"
-  fi
-
-  if [ "X${ENABLE_OPTIMIZED}" == "X1" ]; then
-    CMAKE_BUILD_TYPE="RelWithDebInfo"
-  else
-    CMAKE_BUILD_TYPE="Debug"
-  fi
-
-  # Compute CMake build type
-  cmake \
-    -DLLVM_CONFIG_BINARY="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config" \
-    -DLLVMCC="${KLEE_CC}" \
-    -DLLVMCXX="${KLEE_CXX}" \
-    ${KLEE_Z3_CONFIGURE_OPTION} \
-    ${KLEE_UCLIBC_CONFIGURE_OPTION} \
-    -DGTEST_SRC_DIR=${GTEST_SRC_DIR} \
-    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
-    ${KLEE_ASSERTS_OPTION} \
-    -DENABLE_UNIT_TESTS=TRUE \
-    -DENABLE_SYSTEM_TESTS=TRUE \
-    -DLIT_ARGS="-v" \
-    ${SRC_DIR}/klee
-  make
+if [ "X${ENABLE_OPTIMIZED}" == "X1" ]; then
+  CMAKE_BUILD_TYPE="RelWithDebInfo"
 else
-  # Build KLEE
-  # Note: ENABLE_SHARED=0 is required because llvm-2.9 is incorectly packaged
-  # and is missing the shared library that was supposed to be built that the build
-  # system will try to use by default.
-  ${SRC_DIR}/klee/configure --with-llvmsrc=/usr/lib/llvm-${LLVM_VERSION}/build \
-              --with-llvmobj=/usr/lib/llvm-${LLVM_VERSION}/build \
-              --with-llvmcc=${KLEE_CC} \
-              --with-llvmcxx=${KLEE_CXX} \
-              ${KLEE_Z3_CONFIGURE_OPTION} \
-              ${KLEE_UCLIBC_CONFIGURE_OPTION}
-  make  DISABLE_ASSERTIONS=${DISABLE_ASSERTIONS} \
-        ENABLE_OPTIMIZED=${ENABLE_OPTIMIZED} \
-        ENABLE_SHARED=0
+  CMAKE_BUILD_TYPE="Debug"
 fi
+
+# Compute CMake build type
+cmake \
+  -DLLVM_CONFIG_BINARY="/usr/lib/llvm-${LLVM_VERSION}/bin/llvm-config" \
+  -DLLVMCC="${KLEE_CC}" \
+  -DLLVMCXX="${KLEE_CXX}" \
+  ${KLEE_Z3_CONFIGURE_OPTION} \
+  ${KLEE_UCLIBC_CONFIGURE_OPTION} \
+  -DGTEST_SRC_DIR=${GTEST_SRC_DIR} \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+  ${KLEE_ASSERTS_OPTION} \
+  -DENABLE_UNIT_TESTS=TRUE \
+  -DENABLE_SYSTEM_TESTS=TRUE \
+  -DLIT_ARGS="-v" \
+  ${SRC_DIR}/klee
+make
 
 ###############################################################################
 # Unit tests
 ###############################################################################
-if [ "X${USE_CMAKE}" == "X1" ]; then
-  make unittests
-else
-  # The unittests makefile doesn't seem to have been packaged so get it from SVN
-  sudo mkdir -p /usr/lib/llvm-${LLVM_VERSION}/build/unittests/
-  svn export  http://llvm.org/svn/llvm-project/llvm/branches/${SVN_BRANCH}/unittests/Makefile.unittest \
-      ../Makefile.unittest
-  sudo mv ../Makefile.unittest /usr/lib/llvm-${LLVM_VERSION}/build/unittests/
-
-  make unittests \
-      DISABLE_ASSERTIONS=${DISABLE_ASSERTIONS} \
-      ENABLE_OPTIMIZED=${ENABLE_OPTIMIZED} \
-      ENABLE_SHARED=0
-fi
+make unittests
 
 ###############################################################################
 # lit tests
 ###############################################################################
-if [ "X${USE_CMAKE}" == "X1" ]; then
-  make systemtests
-else
-  # Note can't use ``make check`` because llvm-lit is not available
-  cd test
-  # The build system needs to generate this file before we can run lit
-  make lit.site.cfg \
-      DISABLE_ASSERTIONS=${DISABLE_ASSERTIONS} \
-      ENABLE_OPTIMIZED=${ENABLE_OPTIMIZED} \
-      ENABLE_SHARED=0
-  cd ../
-  lit -v test/
-fi
+make systemtests
--- .travis/llvm_compiler.sh
+++ .travis/llvm_compiler.sh
@@ -11,15 +11,6 @@ if [ "${LLVM_VERSION}" != "2.9" ]; then
     KLEE_CC=/usr/bin/clang-${LLVM_VERSION}
     KLEE_CXX=/usr/bin/clang++-${LLVM_VERSION}
 else
-    # Just use pre-built llvm-gcc downloaded earlier
-    KLEE_CC=${BUILD_DIR}/llvm-gcc/bin/llvm-gcc
-    KLEE_CXX=${BUILD_DIR}/llvm-gcc/bin/llvm-g++
-    export C_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
-    export CPLUS_INCLUDE_PATH=/usr/include/x86_64-linux-gnu
-
-    # Add symlinks to fix llvm-2.9-dev package so KLEE can configure properly
-    # Because of the way KLEE's configure script works this must be a relative
-    # symlink, **not** absolute!
-    test -L '/usr/lib/llvm-2.9/build/Release' || sudo sh -c 'cd /usr/lib/llvm-2.9/build/ && ln -s ../ Release'
-    test -L '/usr/lib/llvm-2.9/build/include' || sudo sh -c 'cd /usr/lib/llvm-2.9/build/ && ln -s ../include include'
+    echo "Unknown LLVM version ${LLVM_VERSION}"
+    exit 1
 fi
