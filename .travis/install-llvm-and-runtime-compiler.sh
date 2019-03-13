c0cc3fa694c32e86c8b1cb82a6d6ad633f2764ad
Martin Nowack <m.nowack@imperial.ac.uk>
2019-03-13 16:23:40 +0000

    Use newer gcc to compile KLEE

--- .travis/install-llvm-and-runtime-compiler.sh
+++ .travis/install-llvm-and-runtime-compiler.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -x
 set -ev
 
+# Install newer compiler to support C++11
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -qq update
+sudo apt-get -qq install g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 50
+
+export CC=gcc-5
+export CXX=g++-5
+
 sudo apt-get install -y llvm-${LLVM_VERSION} llvm-${LLVM_VERSION}-dev
 
 if [ "${LLVM_VERSION}" == "3.4" ]; then
