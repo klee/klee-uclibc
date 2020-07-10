8340cf67367cdd81e12905a38e61279cbc3e6182
Cristian Cadar <c.cadar@imperial.ac.uk>
2020-07-10 18:58:54 +0100

    Get a basic CI working again

--- .travis/testing-utils.sh
+++ .travis/testing-utils.sh
@@ -2,6 +2,11 @@
 # Make sure we exit if there is a failure
 set -e
 
+# Install lit and tabulate
+pip3 install lit
+pip3 install tabulate
+
+
 mkdir ${BUILD_DIR}/test-utils/
 cd ${BUILD_DIR}/test-utils/
 
