fd0d72db501bae2ad5745e66e9107a1ba717a16e
Martin Nowack <m.nowack@imperial.ac.uk>
2020-12-18 16:14:57 +0000

    Remove old CI system

--- .travis/install-llvm-and-runtime-compiler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-set -ev
-
-sudo apt-get install -y llvm-${LLVM_VERSION} llvm-${LLVM_VERSION}-dev
-sudo apt-get install -y clang-${LLVM_VERSION} llvm-${LLVM_VERSION}-tools
-sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${LLVM_VERSION} 20
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${LLVM_VERSION} 20
--- .travis/uclibc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-# Make sure we exit if there is a failure
-set -e
-
-source ${SRC_DIR}/.travis/llvm_compiler.sh
-
-cd ${SRC_DIR}
-./configure --make-llvm-lib --with-cc "${KLEE_CC}" --with-llvm-config /usr/bin/llvm-config-${LLVM_VERSION}
-make
